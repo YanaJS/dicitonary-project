9041811f80824997e3745dbcbe4f1cb0364f588c
YanaJS <yaman.ilirjana@gmail.com>
2023-11-22 13:01:03 +0300

    created the components for the phonetic and installed the phonetic audio to the new component

--- src/Meaning.js
+++ src/Meaning.js
@@ -3,7 +3,7 @@ import Synonyms from "./Synonyms";
 
 
 export default function Meaning(props){
-    console.log(props.meaning);
+    //console.log(props.meaning);
     return (
         <div className = "Meaning">
     
---
+++ src/Phonetic.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactAudioPlayer from 'react-audio-player';
+
+export default function Phonetic(props){
+    //console.log(props.phonetic);
+    return (
+        <div className= "Phonetic">
+            <ReactAudioPlayer
+            src={props.phonetic.audio}
+            autoPlay
+            controls
+            />
+            <br/>
+            {props.phonetic.text}
+        </div>
+    );
+}
