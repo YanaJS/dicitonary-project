c070e3b0d542815343aaf18d9cfad5e4821cf4a9
YanaJS <yaman.ilirjana@gmail.com>
2023-11-26 14:01:52 +0300

    integrated photos API from pexels.com

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 
@@ -10,13 +11,22 @@ export default function Dictionary(props){
     const [keyword, setKeyword] = useState(props.defaultKeyword);
     const [results, setResults] = useState(null);
     const[loaded, setLoaded] = useState(false);
+    const [photos, setPhotos] = useState(null);
 
 
 
-function HandleResponse(response){
+function handleDictionaryResponse(response){
     //console.log(response.data[0]);
     setResults(response.data[0]);
     //console.log(response.data[0].meanings[0].definitions[0].definition);
+
+}
+
+function handlePexelsResponse(response){
+    console.log("#####")
+    console.log(response.data);
+    console.log("<<<<<<<<")
+    setPhotos(response.data.photos);
 
 }
 
@@ -24,7 +34,14 @@ function search(){
     //documentations: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
     //console.log(apiUrl);
-    axios.get(apiUrl).then(HandleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+   
+    let pexelsApiKey = "xUF9ZzXOv1urmkvh4hgkcMOY0VPS96nBhw13pvCZzcIXbDaKG5ZJwcjr";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = {"Authorization" : `${pexelsApiKey}`} ;
+
+    axios.get(pexelsApiUrl, { headers: headers}).then(handlePexelsResponse);
 
 }
 
@@ -65,6 +82,7 @@ if(loaded){
         </div>
         </section>
         <Results  results={results}/>
+        <Photos photos= {photos}/>
 </div>
     );
 }else{
